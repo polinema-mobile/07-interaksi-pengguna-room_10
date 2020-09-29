84c7683c45f23e1cd6ca7e8c44b66dfd8d5a70ce
muhamadalfin <malfin416@gmail.com>
2020-09-29 09:58:04 +0700

    Muhamad Alfin | Membuat Layout (Nama, NIS, Tgl Lahir, Jenis Kelamin, Jurusan, BtnSimpan)

--- Aplikasi Mahasiswa/app/src/main/java/com/aplikasi/mahasiswa/MainActivity.java
+++ Aplikasi Mahasiswa/app/src/main/java/com/aplikasi/mahasiswa/MainActivity.java
@@ -4,11 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private TextView tgl_lahir;
+    private EditText btDatePicker;
+
+    Button simpan;
+    EditText nama_mahasiswa, nis;
+    TextView thasil;
+    RadioGroup jenis_kelamin;
+
+    private void showDateDialog(){
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                tgl_lahir.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        tgl_lahir = (TextView) findViewById(R.id.tgl_lahir);
+        btDatePicker = (EditText) findViewById(R.id.tgl_lahir);
+        btDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateDialog();
+            }
+        });
+
     }
+
+
 }
